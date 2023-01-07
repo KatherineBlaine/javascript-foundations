dc3f8c44046a34ae9072e9fc48b064ec0c7d1dde
Katherine Blaine <blaine.katherine12@gmail.com>
2023-01-07 13:34:00 -0600

    Add completed runner files

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,25 @@
 class Runner {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.milesRun = 0;
+        this.fitness = 25;
+        this.completedRaces = [];
+    }
 
+    runSomeMiles(miles) {
+        this.fitness += miles;
+        this.milesRun += miles;
+    }
+
+    stretch() {
+        this.fitness += .5;
+    }
+
+    runRace(raceName, raceMiles) {
+        this.completedRaces.push(raceName);
+        this.runSomeMiles(raceMiles);
+    }
 }
 
 module.exports = Runner;
