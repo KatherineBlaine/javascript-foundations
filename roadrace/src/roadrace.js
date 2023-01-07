c4867dafd900a53a811cf06f3b8b8e582d216019
Katherine Blaine <blaine.katherine12@gmail.com>
2023-01-07 13:49:52 -0600

    Add completed roadrace test files

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,25 @@
 class Roadrace {
+    constructor(race) {
+        this.name = race.title;
+        this.location = race.city;
+        this.participants = [];
+    }
 
+    setDistance(raceLength) {
+        this.distance = raceLength;
+        return `The ${this.name} in ${this.location} is a ${raceLength} mile race.`
+    }
+
+    registerParticipants(runner) {
+        this.participants.push(runner);
+    }
+
+    completeRace() {
+        for (var i = 0; i < this.participants.length; i ++) {
+            this.participants[i].completedRaces.push(this.name);
+            this.participants[i].milesRun += this.distance;
+        }
+    }
 }
 
 module.exports = Roadrace;
